7bdf61315cf8a18c10c50cc813d671971e147b1a
Vaibhav_07 <138979375+VaibhavS0710@users.noreply.github.com>
2025-06-09 14:38:10 +0530

    Create 234. Plaindrome Linked List.java

---
+++ Linked Lists/234. Plaindrome Linked List.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+        while(f.next!=null && f.next.next!=null){
+            s = s.next;
+            f = f.next.next;
+        }
+
+        ListNode temp = reverse(s.next);
+        s.next = temp;
+        ListNode p1 = head;
+        ListNode p2 = s.next;
+        while(p2!=null){
+            if(p1.val!=p2.val){
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
+    }
+}
