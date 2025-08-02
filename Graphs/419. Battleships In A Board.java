c40eb747f9b39846c74aa443ad14d4816592bb4e
Vaibhav_07 <138979375+VaibhavS0710@users.noreply.github.com>
2025-08-02 13:25:54 +0530

    Create 419. Battleships In A Board.java

---
+++ Graphs/419. Battleships In A Board.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void dfs(char[][] board, int i, int j){
+        if(i<0 || i>=board.length || j<0 || j>=board[i].length || board[i][j]=='.'){
+            return;
+        }
+        board[i][j] = '.';
+        dfs(board, i+1, j);
+        dfs(board, i, j+1);
+    }
+    public int countBattleships(char[][] board) {
+        int count = 0;
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[i].length; j++){
+                if(board[i][j] == 'X'){
+                    count++;
+                    dfs(board, i, j);
+                }
+            }
+        }
+        return count;
+    }
+}
